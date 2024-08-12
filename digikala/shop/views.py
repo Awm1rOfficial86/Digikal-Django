37ab0329dac5de96a2076617fd1bf51c3b14f607
Awm1r Official <amirm4a1@gmail.com>
2024-08-13 00:31:24 +0330

    Add Advertising In Models

--- digikala/shop/views.py
+++ digikala/shop/views.py
@@ -7,14 +7,17 @@ def index(request):
     Slider_Item = SliderItem.objects.all()
     Product_Items = Product.objects.all()
     Category_Items = Category.objects.all()
+    Advertising1_Item = Advertising1.objects.all()
+    Advertising2_Item = Advertising2.objects.all()
     for Item in Product_Items:
-
         Pr_It = Item.Price
         PrOf_It = Item.OfferPrice
         a = PrOf_It / Pr_It
         p = a * 100
         k = 100 - p
-        limit_t = k<=43
+        limit_t = k <= 43
+
+
 
     return render(request, 'main/index.html', {'Slider_Item': Slider_Item, 'Product_Item': Product_Items,
-    'Tkhfif': k, 'limit_t': limit_t, 'Category_Items': Category_Items})
+    'Tkhfif': k, 'limit_t': limit_t, 'Category_Items': Category_Items, 'Advertising1_Item': Advertising1_Item, 'Advertising2_Item': Advertising2_Item})
